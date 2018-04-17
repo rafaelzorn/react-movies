45816803b47cff75ff44047cf97890a31ddaa36f
rafaelzorn <rafael.zorn@gmail.com>
2018-04-16 23:37:47 -0300

    Created .env

--- src/services/api/Movies.js
+++ src/services/api/Movies.js
@@ -1,8 +1,8 @@
-import { http, API_KEY } from './Http';
+import { http } from './Http';
 
 export const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/w500'
 export const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original'
 
 export const getAllMovies = (term) => (
-    http.get(`search/movie?api_key=${API_KEY}&query=${term}`)
+    http.get(`search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${term}`)
 );
